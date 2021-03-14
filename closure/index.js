c6c9c1a40221eca71421c24ee5d7bff3162c44a2
Guilherme Silva <guilherme.gss@outlook.com.br>
2021-03-13 22:03:08 -0300

    update file

--- closure/index.js
+++ closure/index.js
@@ -214,8 +214,8 @@ function cycleIterator(array) {
 
 // /*** Uncomment these to check your work! ***/
 const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
-const getDay = cycleIterator(threeDayWeekend);
-console.log(getDay()); // => should log 'Fri'
+//const getDay = cycleIterator(threeDayWeekend);
+//console.log(getDay()); // => should log 'Fri'
 // console.log(getDay()); // => should log 'Sat'
 // console.log(getDay()); // => should log 'Sun'
 // console.log(getDay()); // => should log 'Fri'
